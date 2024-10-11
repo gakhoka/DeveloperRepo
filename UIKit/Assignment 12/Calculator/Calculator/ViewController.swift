0e02c48c19bf6b48c22bd14e7ad8200244d65e1d
gakhoka <giorgigakhokidze14@gmail.com>
2024-10-11 14:08:43 +0400

    Added gradient

--- UIKit/Assignment 12/Calculator/Calculator/ViewController.swift
+++ UIKit/Assignment 12/Calculator/Calculator/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     private var isColorThemButtonTapped = true
+    private let gradient = CAGradientLayer()
     
     @IBOutlet private weak var numbersView: UIView!
     @IBOutlet private weak var remainderButton: UIButton!
@@ -23,9 +24,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        applyGradient()
         setupUI()
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        updateGradientFrame()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateGradientFrame()
+    }
+    
     private func setupUI() {
         numbersView.layer.cornerRadius = 30
     }
@@ -33,6 +45,29 @@ class ViewController: UIViewController {
     private func changeTheme() {
         colorThemeButton.setImage(UIImage(systemName: isColorThemButtonTapped ? "sun.min" : "moon"), for: .normal)
         view.overrideUserInterfaceStyle = isColorThemButtonTapped ? .dark : .light
+    }
+    
+    private func applyGradient() {
+        let colorOne = UIColor(red: 237/255, green: 14/255, blue: 152/255, alpha: 1.0)
+        let colorTwo = UIColor(red: 254/255, green: 90/255, blue: 46/255, alpha: 1.0)
+        
+        gradient.colors = [colorOne.cgColor, colorTwo.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.cornerRadius = 30
+        resultButton.layer.insertSublayer(gradient, at: 0)
+        resultButton.layer.cornerRadius = 30
+        updateGradientFrame()
+    }
+    
+    private func updateGradientFrame() {
+        gradient.frame = resultButton.bounds
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        coordinator.animate(alongsideTransition: { [weak self] _ in
+            self?.updateGradientFrame()
+        }, completion: nil)
     }
 }
 
