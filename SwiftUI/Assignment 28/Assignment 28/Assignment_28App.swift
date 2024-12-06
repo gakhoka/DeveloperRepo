98c0699fd9fe73a751e43b7d9c0823c9e15e8885
gakhoka <giorgigakhokidze14@gmail.com>
2024-12-06 16:33:31 +0100

    Assignment 28 completed

---
+++ SwiftUI/Assignment 28/Assignment 28/Assignment_28App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_28App.swift
+//  Assignment 28
+//
+//  Created by Giorgi Gakhokidze on 05.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_28App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/Assignment 28/Assignment 28/UIColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColorExtension.swift
+//  Assignment 28
+//
+//  Created by Giorgi Gakhokidze on 05.12.24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let startColor = Color(red: 102/255, green: 90/255, blue: 240/255)
+    static let endColor = Color(red: 167/255, green: 90/255, blue: 178/255)
+    static let imagesColor = Color(red: 98 / 255, green: 72 / 255, blue: 178/255)
+    static let customGreen = Color(red: 0.0, green: 128/255, blue: 1/255)
+}
